530b74ec8189fffc60794c6e8bb543b1597ab467
MxWmr <maximilien.wemaere@gmail.com>
2024-03-20 16:37:23 +0100

    dev ok

--- imu_process.hpp
+++ imu_process.hpp
@@ -1,72 +1,169 @@
-#ifndef DEF_IMU_PROCESS
-#define DEF_IMU_PROCESS
+#pragma once
 #include <vector>
 #include <thread>
 #include "utils.hpp"
 #include <iostream>
 #include <string>
 #include "DataWriter.hpp"
+#include <ceres/ceres.h>
 
 // typedef std::vector<double> Timevect;
 // typedef std::vector<Eigen::Vector3d> Vect3;
+// Residual fonctor of USBL
 
-
-class imu_process{
-    public:
+class imu_process
+{
+public:
     imu_process(std::string path, std::string simu_name);
-    void get_data();
-    void orient_dvl();
-    void orient_imu(const bool inert=false);
+    void get_data(const bool simu);
+    void orient_dvl(const bool simu);
+    void orient_imu(const bool inert = false,const Eigen::Vector3d& bias=Eigen::Vector3d::Zero(), const double scale_factor=1);
     void integrate_dvl();
     void integrate_imu1();
     void integrate_imu2();
-    std::vector<double> imu_rmse();
-    std::vector<double> dvl_rmse();
-    void export_results();
+    void export_results(const bool simu);
+    std::pair<int, int> cutTime(std::vector<double> &time, const double &beginTime, const double &endTime = INFINITY);
+    void keepDataInSameInterval();
+    std::vector<double> rmse();
+    std::vector<double> get_imutime();
+    std::vector<double> get_dvltime();
+    std::vector<double> get_depth_imutime();
+    std::vector<double> get_phinslat_imutime();
+    std::vector<Eigen::Vector3d> get_orientation_imutime();
+    std::vector<Eigen::Vector3d> get_imuaccel_b();
+    std::vector<Eigen::Vector3d> get_dvlspeed_n();
+    Eigen::Vector3d get_initspeed();
+    Eigen::Vector3d get_bias();
+    double get_scale_factor();
+    void find_bias();
 
+    template <typename T>
+    void cutData(std::vector<T> &data, const std::pair<int, int> &interval)
+    {
+        int begin = interval.first;
+        int end = interval.second;
 
-    protected:
+        std::vector<T> newData;
+
+        for (int i(begin); i <= end; i++)
+        {
+            newData.push_back(data[i]);
+        }
+
+        newData.swap(data);
+    }
+
+protected:
     std::string m_path;
     std::string m_simu_name;
     Eigen::Vector3d m_initpos;
     Eigen::Vector3d m_initspeed;
-    Eigen::Matrix<double, 3, 3> m_misalignement;
+    Eigen::Matrix<double, 3, 3> m_misalignement_dvl2b;
+    double m_scale_factor;
+    Eigen::Vector3d m_bias;
 
     std::vector<double> m_depth;
-    std::vector<double> m_depth_dvltime;
+    std::vector<double> m_depth_imutime;
     std::vector<double> m_usbllat;
+    std::vector<Eigen::Vector3d> m_usblgeo;
 
-    std::vector<Eigen::Vector3d> m_orientation;
-    std::vector<Eigen::Vector3d> m_orientation_dvltime;
-    std::vector<Eigen::Vector3d> m_orientation_imutime;
+    std::vector<Eigen::Vector3d> m_orientation_b2n;
+    std::vector<Eigen::Vector3d> m_orientation_b2n_dvltime;
+    std::vector<Eigen::Vector3d> m_orientation_b2n_imutime;
+
+    std::vector<Eigen::Vector3d> m_phinspos;
+    std::vector<Eigen::Vector3d> m_phinspos_dvltime;
+    std::vector<Eigen::Vector3d> m_phinspos_imutime;
+    std::vector<double> m_phinslat;
+    std::vector<double> m_phinslat_imutime;
+
+    std::vector<Eigen::Vector3d> m_dvlspeed_b;
+    std::vector<Eigen::Vector3d> m_dvlspeed_n;
+    std::vector<Eigen::Vector3d> m_dvlspeed_n_imutime;
+    std::vector<Eigen::Vector3d> m_dvlpos;
+
+    std::vector<Eigen::Vector3d> m_imuaccel_b;
+    std::vector<Eigen::Vector3d> m_imuaccel_n;
+    std::vector<Eigen::Vector3d> m_imuspeed_n;
+    std::vector<Eigen::Vector3d> m_imupos;
 
     std::vector<Eigen::Vector3d> m_refpos;
-    std::vector<Eigen::Vector3d> m_refspeed;
-    std::vector<Eigen::Vector3d> m_refspeed_imutime;    
+    std::vector<Eigen::Vector3d> m_refspeed_n;
+    std::vector<Eigen::Vector3d> m_refspeed_n_imutime;    
     std::vector<Eigen::Vector3d> m_refpos_imutime;
-    std::vector<Eigen::Vector3d> m_refspeed_dvltime;
+    std::vector<Eigen::Vector3d> m_refspeed_n_dvltime;
     std::vector<Eigen::Vector3d> m_refpos_dvltime;
     std::vector<double> m_reflat;
     std::vector<double> m_refdepth;
     std::vector<double> m_refdepth_imutime;
     std::vector<double> m_reflat_imutime;
 
-    std::vector<Eigen::Vector3d> m_dvlspeed;
-    std::vector<Eigen::Vector3d> m_dvlpos;
-
-    std::vector<Eigen::Vector3d> m_imuaccel;
-    std::vector<Eigen::Vector3d> m_imuspeed;
-    std::vector<Eigen::Vector3d> m_imupos;
-
-    std::vector<double>  m_dvltime;
-    std::vector<double>  m_depthtime;
-    std::vector<double>  m_imutime;
-    std::vector<double>  m_usbltime;
-    std::vector<double>  m_reftime;
-    std::vector<double>  m_ortime;
 
 
-
+    std::vector<double> m_dvltime;
+    std::vector<double> m_depthtime;
+    std::vector<double> m_imutime;
+    std::vector<double> m_usbltime;
+    std::vector<double> m_phinstime;
+    std::vector<double> m_ortime;
+    std::vector<double> m_reftime;
 };
 
-#endif
+struct Res_bias_sf
+{
+    Res_bias_sf(imu_process &obj, int i)
+    {
+        m_process = &obj;
+        m_i = i;
+    };
+    template <typename T>
+    bool operator()(const T *const bias, const T *const scale_fact, T *residual) const
+    {
+
+        Eigen::Vector3<T> _bias = Eigen::Vector3<T>(bias);
+        T _scale_fact = scale_fact[0];
+
+        std::vector<Eigen::Vector3d> dvlspeed_n = m_process->get_dvlspeed_n();
+        std::vector<Eigen::Vector3d> imuaccel_b = m_process->get_imuaccel_b();
+        std::vector<Eigen::Vector3d> orientation_b2n_imutime = m_process->get_orientation_imutime();
+        std::vector<double> imutime = m_process->get_imutime();
+        std::vector<double> dvltime = m_process->get_dvltime();
+        std::vector<double> depth_imutime = m_process->get_depth_imutime();
+        std::vector<double> phinslat_imutime = m_process->get_phinslat_imutime();
+
+        // Eigen::Vector3<T> werr;
+        Eigen::Vector3<T> werr = {T{0},T{0},T{0}};
+
+        int j(0);
+        while (imutime[j] <= dvltime[m_i - 1]){j++;}
+
+        Eigen::Vector3<T> speedincr;
+        while (imutime[j] < dvltime[m_i] && j < imutime.size())
+        {
+
+            double h = -depth_imutime[j]; // - because depth is not altitude !!
+            double Lat = phinslat_imutime[j] * PI / 180.;
+
+            Eigen::Matrix3d R_b2n = utils::get_rotmat(orientation_b2n_imutime[j]);
+
+            speedincr = 0.02 * (Eigen::Vector3<T>{R_b2n * ((imuaccel_b[j]  - _bias) / _scale_fact)} + utils::get_g(Lat, h));
+            werr -= speedincr;
+
+            j++;
+        }
+        werr += dvlspeed_n[m_i] - dvlspeed_n[m_i - 1];
+
+        if (m_i % 100 == 0)
+        {
+            // std::cout << werr << std::endl;
+        }
+        residual[0] = werr[0];
+        residual[1] = werr[1];
+        residual[2] = werr[2];
+        return true;
+    }
+
+private:
+    imu_process *m_process;
+    int m_i;
+};
--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 
 // include other files
 #include "imu_process.hpp"
-#include "imu_process_real.hpp"
 #include "utils.hpp"
 
 int main(){
 
-    bool simu =false;
+    bool simu =true;
 
     bool optim_bias = true;
     bool inert = false;
@@ -26,7 +25,7 @@ int main(){
 
     if (simu)
     {
-        std::string simu_shape = "SQUARE"; // "LINE" "TEST" "SQUARE" "8TURN" "HELICOIDAL" "TRIANGLE"
+        std::string simu_shape = "TEST"; // "LINE" "TEST" "SQUARE" "8TURN" "HELICOIDAL" "TRIANGLE"
         std::string noise = "0"; // "1" for noise
         simu_name = "SINS_CPP_"+simu_shape+"/"+"SIM_"+noise+"/";
     }
@@ -36,7 +35,7 @@ int main(){
     }
 
 
-    imu_process_real process(path,simu_name);
+    imu_process process(path,simu_name);
 
     // get data from files
     process.get_data(simu);
@@ -71,13 +70,10 @@ int main(){
 
     if(res_rmse){
 
-        std::vector<double> dvl_r = process.dvl_rmse();
-        std::vector<double> imu_r = process.imu_rmse();
+        std::vector<double> imu_r = process.rmse();
 
         std::cout << "RMSE betwwen imu speed and ref speed: "<<imu_r[0]<<std::endl;
-        std::cout << "RMSE betwwen imu pos and ref pos: "<<imu_r[1]<<std::endl;
-        std::cout << "RMSE betwwen dvl pos and ref pos: "<<dvl_r[1]<<std::endl;
-        
+        std::cout << "RMSE betwwen imu pos and ref pos: "<<imu_r[1]<<std::endl;        
     }
 
 
